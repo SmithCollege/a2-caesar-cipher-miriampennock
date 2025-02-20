34ac08013825df1297340c7fd69bccb6592491be
miriampennock9205 <mpennock@smith.edu>
2025-02-20 13:34:31 -0500

    Wrote some javadoc comments

--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -7,26 +7,44 @@ import java.util.Arrays;
 
 public class DynamicArray<T> implements IndexAccess<T>{
     
-    // What instance variables do you need?
+    //Instance Variables
     private Object[] array;
     private int size;
-    // Write a constructor to make a new DynamicArray Object from an array
+
+    //Constructor to make a new DynamicArray Object from an array
     public DynamicArray() {
-        this.array = new Object[10]; //Default initial capacity
+        this.array = new Object[10]; //Default initial capacity, this changed later
         this.size = 0;
     }
 
+    /**
+     * Doubles the capacity of the internal array when it reaches full capacity.
+     */
     public void grow() {
         int newCapacity = array.length * 2;
         array = Arrays.copyOf(array, newCapacity);
     }
 
+    /**
+     * Adds a new element to the end of the dynamic array.
+     * If the array is full, it grows to accommodate more elements.
+     *
+     * @param val The value to add to the array.
+     */
     public void add(T val) {
         if (size == array.length) {
             grow();
         }
         array[size++] = val;
     }
+
+    /**
+     * Retrieves the element at the specified index.
+     *
+     * @param i The index of the element to retrieve.
+     * @return The element at the specified index.
+     * @throws IndexOutOfBoundsException if the index is out of bounds.
+     */
     @SuppressWarnings("unchecked")
     public T get(int i) {
         if (i < 0 || i >= size) {
@@ -35,6 +53,13 @@ public class DynamicArray<T> implements IndexAccess<T>{
         return (T) array[i];
     }
 
+    /**
+     * Retrieves the element at an index offset from a given position.
+     *
+     * @param i The starting index.
+     * @param offset The offset from the starting index.
+     * @return The element at the computed index.
+     */
     @SuppressWarnings("unchecked")
     public T get(int i, int offset) {
         int newIndex = (i + offset) % size;
@@ -42,14 +67,24 @@ public class DynamicArray<T> implements IndexAccess<T>{
         return (T) array[newIndex];
     }
 
+    /**
+     * Sets the value at the specified index.
+     *
+     * @param i The index to set the value at.
+     * @param val The new value to store at the index.
+     * @throws IndexOutOfBoundsException if the index is out of bounds.
+     */
     public void set(int i, T val) {
         if (i < 0 || i >= size) throw new IndexOutOfBoundsException("Index out of bounds");
         array[i] = val;
     }
 
+    /**
+     * Returns the number of elements currently stored in the dynamic array.
+     *
+     * @return The size of the dynamic array.
+     */
     public int size() {
         return size;
     }
-    // Now implement the methods from IndexAccess?
-
 }
