ca3b15a9743b8709c1cd1c9a73bc00dd9603cef4
miriampennock9205 <mpennock@smith.edu>
2025-02-20 13:29:33 -0500

    Working Caesar Cipher...Yay!

--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -2,12 +2,54 @@
 // Classes to build the project
 package a1template;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class DynamicArray<T> implements IndexAccess<T>{
     
     // What instance variables do you need?
-
+    private Object[] array;
+    private int size;
     // Write a constructor to make a new DynamicArray Object from an array
+    public DynamicArray() {
+        this.array = new Object[10]; //Default initial capacity
+        this.size = 0;
+    }
 
+    public void grow() {
+        int newCapacity = array.length * 2;
+        array = Arrays.copyOf(array, newCapacity);
+    }
+
+    public void add(T val) {
+        if (size == array.length) {
+            grow();
+        }
+        array[size++] = val;
+    }
+    @SuppressWarnings("unchecked")
+    public T get(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) array[i];
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int i, int offset) {
+        int newIndex = (i + offset) % size;
+        if (newIndex < 0) newIndex += size; // Handle negative indices
+        return (T) array[newIndex];
+    }
+
+    public void set(int i, T val) {
+        if (i < 0 || i >= size) throw new IndexOutOfBoundsException("Index out of bounds");
+        array[i] = val;
+    }
+
+    public int size() {
+        return size;
+    }
     // Now implement the methods from IndexAccess?
 
 }
